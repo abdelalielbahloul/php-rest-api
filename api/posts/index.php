e832b9b5c04e6cb08ce7b7e17e134304d04dfb33
Abdelali EL BAHLOUL <abdelali.elbahloul@regsquare.com>
2020-03-21 02:58:33 +0100

    feat : fetch result

---
+++ api/posts/index.php
@@ -0,0 +1,38 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+require_once '../../config/Database.php';
+require_once '../../models/Post.php';
+
+// connect to Db
+$database = new Database();
+$db = $database->connect();
+
+// Intanciate the Post Class
+$post = new Post($db);
+
+// get Results 
+$result = $post->index();
+$num = $result->rowCount();
+
+if ($num > 0) {
+    $posts = array();
+    $posts['data'] = array();
+    
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+
+        $items = array(
+            'id' => $id,
+            'title' => $title,
+            'body' => $body
+        );
+
+        // push the items into posts array
+        array_push($posts['data'], ...$items);
+    }
+    // return the json to the client 
+    echo json_encode($posts);
+} else {
+    echo json_encode(array('message' => 'No data found'));
+}
