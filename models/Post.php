caa185d6f2c1b263f749995555ffd81b396a9aa4
Abdelali EL BAHLOUL <abdelali.elbahloul@regsquare.com>
2020-03-21 15:23:12 +0100

    feat : add show method to get single post by id

--- models/Post.php
+++ models/Post.php
@@ -20,4 +20,17 @@
 
         return $stmt;
     }
+
+    public function show() {
+        $query = 'SELECT title,body FROM '. $this->table .' WHERE id = ?';
+        $stmt = $this->cnx->prepare($query);
+        $stmt->bindParam(1, $this->id);
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $this->title = $row['title'];
+        $this->body = $row['body'];
+
+    }
  } 
